e469639f11a7b4b94358f4b7952ad9e5e79b90a6
Albert Patterson <apatters@mathworks.com>
2017-05-24 19:43:00 -0400

    enabling concurrent view tests on chrome

--- spec/view/runViewTests.js
+++ spec/view/runViewTests.js
@@ -1,27 +1,23 @@
 // create the resultsPromise
 const ResultsPromise = require('../tools/utils/ResultsPromise');
 
-
 // paths of the spec runner files to include in the test suite
 const specSuite =   [
                         '/spec/view/specs/displayPostsUnit.html',
                         '/spec/view/specs/displayPostsUnit.html',
                         '/spec/view/specs/displayPostsUnit.html',
                         '/spec/view/specs/displayPostsUnit.html',
-                        // '/spec/view/specs/displayPostsUnit.html',
-                        // '/spec/view/specs/displayPostsUnit.html',
-                        // '/spec/view/specs/displayPostsUnit.html',
-                        // '/spec/view/specs/displayPostsUnit.html',
+                        '/spec/view/specs/displayPostsUnit.html',
+                        '/spec/view/specs/displayPostsUnit.html',
+                        '/spec/view/specs/displayPostsUnit.html',
+                        '/spec/view/specs/displayPostsUnit.html',
                     ];
 
 // create a tester for the chrome browser
 const chromeTestProcessArgs = require('../tools/utils/chromeTestProcessArgs');
 const ChromeTester = require('../tools/testers/ChromeTester');
 
-// create a tester for the firefox browser
-const firefoxTestProcessArgs = require('../tools/utils/FirefoxTestProcessArgs');
-const FirefoxTester = require('../tools/testers/FirefoxTester');
-
+// the function used to process test results
 const processFcn = function(results){
         if(results.complete){
             console.log('Test point complete:')
@@ -31,24 +27,9 @@ const processFcn = function(results){
         console.log(results.data);
 }
 
+// factories
 const resultsPromiseFactory = ()=>new ResultsPromise(processFcn);
 const chromeTesterFactory = ()=>new ChromeTester(chromeTestProcessArgs);
-const firefoxTesterFactory = ()=>new FirefoxTester(firefoxTestProcessArgs);
-
-
-// const resultsPromiseFactory = function(){
-//     var resultsPromise = new ResultsPromise();
-//     resultsPromise.processFcn = function(results){
-//         if(results.complete){
-//             console.log('Test point complete:')
-//         }else{
-//             console.log('Test point incomplete:')
-//         }
-//         console.log(results.data);
-//     }
-//     return resultsPromise;
-// }
-
 
 // create the app that provides routing to source and test resources
 const testAppFactory = require('../tools/utils/testAppFactory');
@@ -58,25 +39,29 @@ const testApp = testAppFactory();
 const ServerTester = require('../tools/testers/ServerTester');
 const serverTester = new ServerTester(testApp);
 
-
-
-
-// create the system level test runner
+// create the view test runner
 const ViewTestSuiteRunner = require('../tools/runners/ViewTestSuiteRunner');
 const viewTestSuiteRunner = new ViewTestSuiteRunner(specSuite);
 
-// set the necessary testers and options of the system level test runner
+// set the necessary testers and options of the view test runner
 viewTestSuiteRunner.serverTester = serverTester;
 viewTestSuiteRunner.resultsPromiseFactory = resultsPromiseFactory;
 viewTestSuiteRunner.browserTesterFactory = chromeTesterFactory;
+viewTestSuiteRunner.options = {
+    // create a user data directory for the testpoint
+    pointSetup: function(idx){
+        return  ChromeTester.getUserDataDir(idx)
+                .then(function(dir){
+                    const userDataDirDef = `--user-data-dir=${dir}`;
+                    this.browserTesters[idx].processArgs = [...chromeTestProcessArgs, userDataDirDef];
+                }.bind(this));
+    }.bind(viewTestSuiteRunner),
+
+    // remove the user data directory create for the testpoint
+    pointTeardown: function(idx){
+        return ChromeTester.removeUserDataDir(idx);
+    }.bind(viewTestSuiteRunner)
+};
 
 // run the tests on chrome
 viewTestSuiteRunner.run()
-.then(function(){
-    // run the tests on firefox
-    const viewTestSuiteRunnerFF = new ViewTestSuiteRunner(specSuite);
-    viewTestSuiteRunnerFF.serverTester = serverTester;
-    viewTestSuiteRunnerFF.resultsPromiseFactory = resultsPromiseFactory;
-    viewTestSuiteRunnerFF.browserTesterFactory = firefoxTesterFactory;
-    viewTestSuiteRunnerFF.run();
-})
